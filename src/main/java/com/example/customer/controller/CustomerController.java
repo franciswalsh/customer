91437ea282e1b787b23fe19697403f0da8ab12f4
Frank <francisw396@gmail.com>
2017-08-09 13:32:26 -0400

    working front end

--- src/main/java/com/example/customer/controller/CustomerController.java
+++ src/main/java/com/example/customer/controller/CustomerController.java
@@ -5,6 +5,7 @@ import com.example.customer.model.Customer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -20,7 +21,12 @@ public class CustomerController {
     @Autowired
     CustomerServiceImpl customerDatabase;
 
-
+    @RequestMapping(path = "/customer/{id}", method = RequestMethod.GET)
+    public String getCustomer(@PathVariable("id") int id, Model model){
+       Customer customer = customerDatabase.getCustomerById(id);
+       model.addAttribute("customer", customer);
+       return "customer";
+    }
 
     @RequestMapping(path = "/customers", method = RequestMethod.GET)
     public String getCustomers(Model model){
